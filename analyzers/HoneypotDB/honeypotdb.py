1c5c5f14beeeeca28fbee67761796e905a5ce8cc
Brad King <32899553+32bitbradley@users.noreply.github.com>
2024-10-17 13:58:49 +0100

    Update honeypotdb.py

--- analyzers/HoneypotDB/honeypotdb.py
+++ analyzers/HoneypotDB/honeypotdb.py
@@ -13,52 +13,57 @@ class HoneypotDBAnalyzer(Analyzer):
     def __init__(self):
         Analyzer.__init__(self)
 
+    def summary(self, raw):
+        taxonomies = []
+
+        if 'api_response' in raw:
+
+            if len(raw['api_response']['data']) > 0:
+
+                level = 'malicious'
+                namespace = 'HoneypotDB'
+                predicate = 'Score'
+                value = raw['api_response']['data'][0]['score']
+
+                taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
+            else:
+                taxonomies.append(self.build_taxonomy('safe', 'HoneypotDB', 'Score', 0))
+        else:
+            taxonomies.append(self.build_taxonomy('safe', 'HoneypotDB', 'Score', 0))
+
+        return {"taxonomies": taxonomies}
+
     def run(self):
 
         try:
             if self.data_type == "ip":
-                api_key = self.get_param('config.API_KEY', None, 'Missing HoneypotDB API key')
+                api_key = self.get_param('config.API-KEY', None, 'Missing HoneypotDB API key')
 
                 indicator = self.get_data()
 
                 url = 'https://api.honeypotdb.com/score/scores'
                 headers = {
                     'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded',
                     'X-API-KEY': '%s' % api_key }
                 
                 params = {'indicator': indicator}
 
                 response = requests.get(url, headers=headers, params=params)
 
-                if response.ok:
+                if not response.ok:
                     self.error('Failed to query HoneypotDB API\n{}'.format(response.text))
 
                 json_response = response.json()
 
+                if not json_response['success']:
+                    self.error('HoneypotDB API did not respond with success\n{}'.format(response.text))
 
-                self.report({'values': json_response})
+
+                self.report({'api_response': json_response})
             else:
                 self.notSupported()
         except Exception as e:
             self.unexpectedError(e)
-
-    def summary(self, raw):
-        taxonomies = []
-
-        if (raw and 'values' in raw) and (len(raw['data'][0]['score']) > 0):
-
-            taxonomies = []
-            level = 'malicious'
-            namespace = 'HoneypotDB'
-            predicate = 'Score'
-            value = raw['data'][0]['score']
-
-            taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
-        else:
-            taxonomies.append(self.build_taxonomy('safe', 'HoneypotDB', 'Score', 0))
-
-        return {"taxonomies": taxonomies}
 
 
 if __name__ == '__main__':
